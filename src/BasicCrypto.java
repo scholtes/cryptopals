5f4ff8b8b81c856b6234210e339a5083a8175750
scholtes <scholtes.garrett@gmail.com>
2017-07-20 20:17:48 -0500

    Set 1 Chapter 3 -- Complete, with bug fixes and tests Frequency counting was wrong. Fixed

--- src/BasicCrypto.java
+++ src/BasicCrypto.java
@@ -5,8 +5,13 @@ import java.math.BigInteger;
 import java.util.Base64;
 import java.util.Collections;
 import java.io.UnsupportedEncodingException;
+import java.lang.Math;
 
 public class BasicCrypto {
+
+    static final int ALPHA_SIZE = 26;
+    static final int LETTER_A = (int)('A');
+    static final double[] FREQUENCIES_EN = {8.167,1.492,2.782,4.253,12.702,2.228,2.015,6.094,6.966,0.153,0.772,4.025,2.406,6.749,7.507,1.929,0.095,5.987,6.327,9.056,2.758,0.978,2.360,0.150,1.974,0.074};
 
     public static String bigInt2base64(BigInteger bigInt) {
         return new String(Base64.getEncoder().encode(bigInt.toByteArray()));
@@ -46,4 +51,63 @@ public class BasicCrypto {
         }
         return hex.toString();
     }
+
+    // Takes a hex string cipher text and a single character key, and creates a
+    // hex string key whose length is equal to the cipher text
+    public static String repeatSingleByteKey(char key, String hexCipher) throws Exception {
+        String asciiKey = String.join("", Collections.nCopies(hexCipher.length()/2, Character.toString(key)));
+        String hexKey = ascii2hex(asciiKey);
+        return hexKey;
+    }
+
+    // Decrypt a string with a repeating single byte key, with the input string in the hex encoded format, and the
+    // output string in the ascii format.  This is symmetric, with the exception of the ascii/hex conversion.
+    public static String decryptSingleByteXor(String hexCipher, char key) throws Exception {
+        String repeatedKey = repeatSingleByteKey(key, hexCipher);
+        return hex2ascii(xor(repeatedKey, hexCipher));
+    }
+
+    // Encrypt a string with a repeating single byte key, with the input string in ASCII format, and the
+    // output string in the hex encoded format.
+    public static String encryptSingleByteXor(String asciiPlaintext, char key) throws Exception {
+        String hexPlaintext = ascii2hex(asciiPlaintext);
+        String repeatedKey = repeatSingleByteKey(key, hexPlaintext);
+        return xor(repeatedKey, hexPlaintext);
+    }
+
+    // Attempts to find the most likely key based on character frequency analysis on the English alphabet
+    public static char autoGetKey(String hexCipher) throws Exception {
+        double[] frequencies = new double[ALPHA_SIZE];
+        char[] plaintext;
+        double best_score = (double) ALPHA_SIZE;
+        char best_key = (char) 0;
+        double score;
+        double norm;
+        for(int i = 0; i < 0x100; i++) {
+            score = 0;
+            norm = 0.0000001;
+            plaintext = decryptSingleByteXor(hexCipher, (char)(i)).toUpperCase().toCharArray();
+            frequencies = new double[ALPHA_SIZE];
+            //System.out.println(plaintext);
+            for(int k = 0; k < plaintext.length; k++) {
+                if ((LETTER_A <= (int)plaintext[k]) && ((int)plaintext[k] < LETTER_A + ALPHA_SIZE)) {
+                    frequencies[(int)plaintext[k] - LETTER_A]++;
+                }
+            }
+            for(int k = 0; k < ALPHA_SIZE; k++) {
+                norm += frequencies[k];
+            }
+            for(int k = 0; k < ALPHA_SIZE; k++) {
+                score += Math.abs(frequencies[k]/norm - FREQUENCIES_EN[k]/100);
+            }
+            //System.out.println("" + Integer.toString(i, 16) + " : " +  plaintext.length + " : " + score);
+            if (best_score > score) {
+                best_score = score;
+                best_key = (char)(i);
+            }
+        }
+        return best_key;
+    }
+
+
 }
--- src/Set1Chapter3.java
+++ src/Set1Chapter3.java
@@ -7,14 +7,49 @@ import java.math.BigInteger;
 public class Set1Chapter3 {
 
     public static void main(String[] args) throws Exception {
+        challengeTest();
+    }
+
+    public static void challengeTest() throws Exception {
+        String input = new String("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+        char guessedKey = BasicCrypto.autoGetKey(input);
+        String plaintext = BasicCrypto.decryptSingleByteXor(input, guessedKey);
+        System.out.println(input);
+        System.out.println(plaintext);
+    }
+
+    public static void hobbitTest() throws Exception {
+        // Here is a test plaintext
+        String input = new String("It had a perfectly round door like a porthole, painted green, with a shiny yellow brass knob in the exact middle. The door opened on to a tube-shaped hall like a tunnel: a very comfortable tunnel without smoke, with panelled walls, and floors tiled and carpeted, provided with polished chairs, and lots and lots of pegs for hats and coats - the hobbit was fond of visitors. The tunnel wound on and on, going fairly but not quite straight into the side of the hill - The Hill, as all the people for many miles round called it - and many little round doors opened out of it, first on one side and then on another. No going upstairs for the hobbit: bedrooms, bathrooms, cellars, pantries (lots of these), wardrobes (he had whole rooms devoted to clothes), kitchens, dining-rooms, all were on the same floor, and indeed on the same passage. The best rooms were all on the left-hand side (going in), for these were the only ones to have windows, deep-set round windows looking over his garden, and meadows beyond, sloping down to the river. ");
+        // And here is a test cipher text, encrypted with some arbitrary key (some char between 0 and FF... in this case, '[' == 5b)
+        String encryptedInput = BasicCrypto.encryptSingleByteXor(input, '[');
+
+        // Attempt to guess the key from the cipher text alone
+        char guessedKey = BasicCrypto.autoGetKey(encryptedInput);
+        // Guess the plaintext using the guessed key
+        String guessedPlaintext = BasicCrypto.decryptSingleByteXor(encryptedInput, guessedKey);
+
+        if (guessedPlaintext.equals(input)) {
+            System.out.println("Hobbit test passed!");
+        } else {
+            System.out.println("Hobit test failed!");
+        }
+    }
+
+    public static void randomStuffTest() throws Exception {
         String input = new String("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
         String hexKey;
-        String asciiKey;
         for (int i = 0; i < 0x100; i++) {
-            asciiKey = String.join("", Collections.nCopies(input.length()/2, Character.toString((char)(i))));
-            hexKey = BasicCrypto.ascii2hex(asciiKey);
-            //System.out.println((new Integer(i)).toString() + " : " + hexKey.equals(BasicCrypto.ascii2hex(BasicCrypto.hex2ascii(hexKey))));
-            System.out.println(BasicCrypto.hex2ascii(BasicCrypto.xor(hexKey, input)));
+            // Very messy output.... visualize all possible plaintexts, just for some validation
+            System.out.println(Integer.toString(i, 16) + " : " + BasicCrypto.decryptSingleByteXor(input, (char)(i)));
+
+            // This is just some validation that the encryption and decryption are workign properly... they should always exactly undo each other
+            if (input.equals(BasicCrypto.encryptSingleByteXor(BasicCrypto.decryptSingleByteXor(input, (char)(i)), (char)(i)))) {
+                System.out.println("pass");
+            } else {
+                System.out.println("FAIL!!!!!!!!!!!!!!!!!");
+            }
         }
+        System.out.println((int)BasicCrypto.autoGetKey(input));
     }
 }
